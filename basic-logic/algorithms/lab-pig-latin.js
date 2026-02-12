84ac22376afcd0df88a8e8a1a50c639c5a53719f
DanielZG <zam.zagdusa@gmail.com>
2026-02-11 23:23:13 -0800

    feat: add pig latin translator algorithm lab

---
+++ basic-logic/algorithms/lab-pig-latin.js
@@ -0,0 +1,35 @@
+const translatePigLatin = (str) => {
+let pigLatinWord = ""
+const vowelIndex = str.search(/[aeiou]/)
+
+if (vowelIndex === -1){
+  pigLatinWord = str + "ay"
+  } else if (vowelIndex === 0){
+   pigLatinWord = str + "way"
+ } else {
+  pigLatinWord = `${str.slice(vowelIndex)}${str.slice(0, vowelIndex)}ay`
+}
+
+
+return pigLatinWord
+}
+
+
+
+const translatePigLatin1 = (str) => {
+let pigLatinWord = ""
+if (str.indexOf(str.match(/[aeiou]/)) === -1){pigLatinWord = str + "ay"}
+if (str.indexOf(str.match(/[aeiou]/)) === 0){pigLatinWord = str + "way"}
+if (str.indexOf(str.match(/[aeiou]/)) > 0){
+  const cluster = str.slice(0, (str.indexOf(str.match(/[aeiou]/))) )
+  const rest = str.slice((str.indexOf(str.match(/[aeiou]/))), str.length)
+  pigLatinWord = rest + cluster + "ay"
+}
+
+return pigLatinWord
+}
+
+
+
+
+
