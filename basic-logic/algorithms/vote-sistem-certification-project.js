c371403902fdfc28198fd11a90fa970184ff772d
DanielZG <zam.zagdusa@gmail.com>
2026-02-25 23:37:45 -0800

    added Certification Project of a vote sistem

---
+++ basic-logic/algorithms/vote-sistem-certification-project.js
@@ -0,0 +1,51 @@
+const poll = new Map
+
+function addOption(option) {
+    let optionString = "";
+    if (!option) {
+        return optionString = "Option cannot be empty.";
+    };
+    if (!poll.has(option)) {
+        poll.set(option, new Set); optionString = `Option "${option}" added to the poll.`;
+    } else {
+        optionString = `Option "${option}" already exists.`;
+    }
+    return optionString;
+}
+
+const vote = (option, voterId) => {
+    let voteString = ""
+    if (!poll.has(option)) {
+        return voteString = `Option "${option}" does not exist.`
+    } else if
+        (poll.get(option).has(voterId)) {
+        voteString = `Voter ${voterId} has already voted for "${option}".`
+    } else {
+        voteString = `Voter ${voterId} voted for "${option}".`; poll.get(option).add(voterId)
+    }
+
+    return voteString
+}
+
+addOption("Mexico")
+addOption("Argentina")
+addOption("Chile")
+addOption("Ecuador")
+addOption("Guatemala")
+addOption("Puerto Rico")
+
+vote("Mexico", 117)
+vote("Mexico", 109)
+vote("Mexico", 101)
+
+const displayResults = () => {
+    let resultString = "Poll Results:"
+    poll.forEach((votes, option) => {
+        resultString += `\n${option}: ${votes.size} votes`
+    })
+    return resultString
+}
+
+
+
+
