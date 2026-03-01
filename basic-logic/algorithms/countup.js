caf16663eb5623f41a74dc6095762b1fbb6a865d
DanielZG <zam.zagdusa@gmail.com>
2026-02-28 23:30:00 -0800

    feat: add countup function for recursive counting

---
+++ basic-logic/algorithms/countup.js
@@ -0,0 +1,13 @@
+function countup(number) {
+
+  if (number < 1) {
+    return []};
+ 
+    let arr = countup(number - 1);
+    
+    return [...arr, number];
+  
+}
+console.log(countup(5));  
+
+countup(5);
